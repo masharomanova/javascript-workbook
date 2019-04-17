3aabb7307af5752e9ffcd5465bb2b158a6dc7edc
masharomanova <mariavromanova@yahoo.com>
2019-04-17 16:22:14 -0500

    finished pig latin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -14,15 +14,38 @@ function pigLatin(word) {
     Lower case and trim your word using string methods (this will pass some of the tests that are written at the bottom of the file)
     */
    let trimWord = word.trim().toLowerCase();
-   
+   let wordArr = trimWord.split("");
+    console.log(wordArr);
 
     /* 
     IF the first letter is a vowel (you will want to use array and string methods to check), we simply need to return the word with 'yay' on the end 
     */
-    
+
+  let vowelArr = ['a','e','i','o','u']
+
+  if (vowelArr.includes(wordArr[0])) {
+    wordArr.push('y','a','y');
+    console.log(wordArr);
+    let wordLatin = wordArr.join("");
+    return wordLatin;
+  }
     /* 
     ELSE since the first letter is NOT a vowel, we need to LOOP through our word to find the first vowel. Once we find it we need to stop the loop and slice the string at that index so that all consonants before the vowel are moved to the end of the word and then 'ay' is added to the end of that.
     */
+   else {
+     for (let i=0; i<wordArr.length; i++) {
+        if (vowelArr.includes(wordArr[i])) {
+          let newArr = wordArr.splice(0,i);
+          console.log(newArr);
+          let newWordArr = wordArr.concat(newArr);
+          console.log(newWordArr);
+          newWordArr.push('a','y');
+          console.log(newWordArr);
+          let wordLatin = newWordArr.join("");
+          return wordLatin;
+        }
+     }
+   }
 
 }
 
