e8e8b56476134df29546b2ba582b7e5a2a23fc7e
masharomanova <mariavromanova@yahoo.com>
2019-04-28 16:27:27 -0500

    finished towers with read me and new tests created

--- 03week/towersOfHanoi/towersOfHanoi.js
+++ 03week/towersOfHanoi/towersOfHanoi.js
@@ -19,24 +19,46 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startArr, endArr) {
   // Your code here
+  const lastItem = startArr.pop();
+  const newSpot = endArr.push(lastItem)
+  return newSpot
+  }
+  
 
-}
-
-function isLegal() {
+function isLegal(startArr, endArr) {
   // Your code here
-
+  
+  let pickUp = startArr[startArr.length - 1] // this is the last number of the array
+  let putDown = endArr[endArr.length - 1] // this is the last number of the array
+  if (putDown >= pickUp || putDown == undefined) {
+    return true
+  } else {
+    return false 
+  }
 }
 
 function checkForWin() {
   // Your code here
-
+  if (stacks['b'].length === 4 || stacks['c'].length === 4) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+  const startArr = stacks[startStack]
+  const endArr = stacks[endStack]
+   isLegal(startArr, endArr);
+  if (isLegal(startArr, endArr)) {
+    movePiece(startArr, endArr);
+    if (checkForWin()) {
+      return 'WINNER!'
+   }
+  };
 }
 
 function getPrompt() {
@@ -63,11 +85,11 @@ if (typeof describe === 'function') {
   describe('#isLegal()', () => {
     it('should not allow an illegal move', () => {
       stacks = {
-        a: [4, 3, 2],
+        a: [4, 3],
         b: [1],
-        c: []
+        c: [2]
       };
-      assert.equal(isLegal('a', 'b'), false);
+      assert.equal(isLegal('c', 'b'), false);
     });
     it('should allow a legal move', () => {
       stacks = {
@@ -77,12 +99,28 @@ if (typeof describe === 'function') {
       };
       assert.equal(isLegal('a', 'c'), true);
     });
+    it('should not allow input other than a,b,c', () => {
+      stacks = {
+        a: [4, 3, 2, 1],
+        b: [],
+        c: []
+      };
+      assert.equal(isLegal('g', 'a'), false);
+    });
   });
   describe('#checkForWin()', () => {
     it('should detect a win', () => {
       stacks = { a: [], b: [4, 3, 2, 1], c: [] };
       assert.equal(checkForWin(), true);
       stacks = { a: [1], b: [4, 3, 2], c: [] };
+      assert.equal(checkForWin(), false);
+    });
+    it('should not allow a win on column A', () => {
+      stacks = { a: [4, 3, 2, 1], b: [], c: [] };
+      assert.equal(checkForWin(), false);
+    });
+    it('should not allow a win if numbers are not in descending order', () => {
+      stacks = { a: [4, 3, 1, 2], b: [], c: [] };
       assert.equal(checkForWin(), false);
     });
   });
